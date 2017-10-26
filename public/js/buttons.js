9ce553c9d1fa9415f0e42721f5d953a71722e7b2
Nicole W <nicolemairead4@gmail.com>
2017-10-26 12:51:00 -0400

    initial commit

---
+++ public/js/buttons.js
@@ -0,0 +1,395 @@
+a.classList.remove('hide');b.classList.remove('hide');c.classList.remove('hide');d.classList.remove('hide');e.classList.remove('hide');f.classList.remove('hide');g.classList.remove('hide');h.classList.remove('hide');i.classList.remove('hide');
+j.classList.remove('hide');k.classList.remove('hide');l.classList.remove('hide');m.classList.remove('hide');n.classList.remove('hide');o.classList.remove('hide');p.classList.remove('hide');q.classList.remove('hide');r.classList.remove('hide');
+s.classList.remove('hide');t.classList.remove('hide');u.classList.remove('hide');v.classList.remove('hide');w.classList.remove('hide');x.classList.remove('hide');y.classList.remove('hide');z.classList.remove('hide');
+
+
+function hidea() {
+  let letter = 'A';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+  showA.classList.remove('hidden'); a.classList.add('hide');
+  //showAndHide(letter);
+  updateState(currentWord, word, letter);
+
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hideb() {
+  let letter = 'B'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showB.classList.remove('hidden'); b.classList.add('hide');
+    updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidec() {
+  let letter = 'C'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showC.classList.remove('hidden'); c.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hided() {
+  let letter = 'D'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showD.classList.remove('hidden'); d.classList.add('hide');
+    updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidee() {
+  let letter = 'E'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showE.classList.remove('hidden'); e.classList.add('hide');
+    updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidef() {
+  let letter = 'F'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showF.classList.remove('hidden'); f.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hideg() {
+  let letter = 'G'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showG.classList.remove('hidden'); g.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hideh() {
+  let letter = 'H'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showH.classList.remove('hidden'); h.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidei() {
+  let letter = 'I'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showI.classList.remove('hidden'); i.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidej() {
+  let letter = 'J'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showJ.classList.remove('hidden'); j.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidek() {
+  let letter = 'K'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showK.classList.remove('hidden'); k.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidel() {
+  let letter = 'L'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showL.classList.remove('hidden'); l.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidem() {
+  let letter = 'M'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showM.classList.remove('hidden'); m.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hiden() {
+  let letter = 'N'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showN.classList.remove('hidden'); n.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hideo() {
+  let letter = 'O'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showO.classList.remove('hidden'); o.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidep() {
+  let letter = 'P'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showP.classList.remove('hidden'); p.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hideq() {
+  let letter = 'Q'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showQ.classList.remove('hidden'); q.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hider() {
+  let letter = 'R'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showR.classList.remove('hidden'); r.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hides() {
+  let letter = 'S'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showS.classList.remove('hidden'); s.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidet() {
+  let letter = 'T'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showT.classList.remove('hidden'); t.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hideu() {
+  let letter = 'U'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showU.classList.remove('hidden'); u.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidev() {
+  let letter = 'V'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showV.classList.remove('hidden'); v.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidew() {
+  let letter = 'W'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showW.classList.remove('hidden'); w.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidex() {
+  let letter = 'X'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showX.classList.remove('hidden'); x.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidey() {
+  let letter = 'Y'; var newWord = '';
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showY.classList.remove('hidden'); y.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+
+function hidez() {
+  let letter = 'Z'; var newWord = ''; var oldWord = returnedWord;
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+
+  showZ.classList.remove('hidden'); z.classList.add('hide');
+  updateState(currentWord, word, letter);
+  turnsLeft.textContent = 'Turns Left: ' + counter; if(currentWord.join("") === word){alert('You won!')}
+
+  if (counter <= 0) {
+    guessingWord.textContent = 'You lost and the word was ' + word;
+  }
+}
+//}
---
+++ public/js/index.js
@@ -0,0 +1,60 @@
+var howIMetYourMother = document.getElementById('himym');;
+var theOffice = document.getElementById('office');
+var friends = document.getElementById('friends');
+var guessingWord = document.getElementById('the-word');
+var turnsLeft = document.getElementById('turn-count');
+var counter = 10;
+var characters;
+var word;
+var button;
+var currentState;
+var currentWord;
+
+let friendsCharacters = ['ROSS', 'RACHEL', 'MONICA', 'CHANDLER', 'JOEY', 'PHOEBE', 'URSULA', 'SQUIRRELYJOE', 'MRHECKLES', 'TAG', 'GUNTHER', 'MIKE'];
+let howIMetYourMotherCharacters = ['BARNEY', 'TED', 'ROBIN', 'MARSHALL', 'LILY', 'RANJIT', 'THEMOTHER'];
+let theOfficeCharacters = ['MICHAEL', 'JIM', 'DWIGHT', 'PAM', 'PHYLLIS', 'DARRYL', 'ANDY', 'KELLY', 'RYAN', 'KEVIN', 'STANLEY', 'HOLLY', 'OSCAR', 'ANGELA', 'ERIN'];
+
+function renderState(word){
+  return guessingWord.textContent = [...word].fill("_").join("");
+}
+
+function updateState(test, word, letter){
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      test[i] = letter;
+    }
+    if(test.includes(letter)){
+      counter = counter;
+    } else {
+      counter--;
+    }
+  }
+  guessingWord.textContent = test.join("");
+}
+
+function showAndHide(letter){
+  let currentLetter = letter;
+  let lowercaseletter = currentLetter.toLowerCase();
+  let lowerCase = document.querySelector('#lowercaseletter');
+  let showLetter = document.querySelector('#show'+letter);
+  showLetter.classList.remove('hidden');
+  lowerCase.classList.add('hide');
+}
+
+function startGame(charList) {
+  characters = charList;
+  word = characters[Math.floor((Math.random() * characters.length) + 1)];
+  currentState = renderState(word);
+  currentWord = [...currentState];
+  turnsLeft.textContent = 'Turns Left: ' + counter;
+}
+
+howIMetYourMother.addEventListener('click', function() {
+  startGame(howIMetYourMotherCharacters);
+})
+theOffice.addEventListener('click', function() {
+  startGame(theOfficeCharacters);
+})
+friends.addEventListener('click', function() {
+  startGame(friendsCharacters);
+})
