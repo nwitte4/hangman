32ab51e52872bf0e1771afbc7c87b81ece0f40f8
Nicole W <nicolemairead4@gmail.com>
2017-12-21 12:01:38 -0500

    refactor hideLetter function

--- public/js/index.js
+++ public/js/index.js
@@ -5,13 +5,9 @@ var guessingWord = document.getElementById('theword');
 var turnsLeft = document.getElementById('turn-count');
 var themes = document.getElementById('themes');
 var backgroundimg = document.getElementById('backgroundimg');
-var counter;
-var characters;
-var word;
-var button;
-var currentState;
-var currentWord;
+var counter, characters, word, currentState, currentWord;
 
+// character themes
 let friendsCharacters = ['ROSS', 'RACHEL', 'MONICA', 'CHANDLER', 'JOEY', 'PHOEBE', 'URSULA', 'SQUIRRELYJOE', 'MRHECKLES', 'TAG', 'GUNTHER', 'MIKE'];
 let howIMetYourMotherCharacters = ['BARNEY', 'TED', 'ROBIN', 'MARSHALL', 'LILY', 'RANJIT', 'THEMOTHER', 'GARYBLAUMAN', 'WENDYTHEWAITRESS', 'PATRICE', 'SANDYRIVERS', 'SCOOTER', 'VICTORIA', 'ZOEY', 'CARLTHEBARTENDER'];
 let theOfficeCharacters = ['MICHAEL', 'JIM', 'DWIGHT', 'PAM', 'PHYLLIS', 'DARRYL', 'ANDY', 'KELLY', 'RYAN', 'KEVIN', 'STANLEY', 'HOLLY', 'OSCAR', 'ANGELA', 'ERIN', 'TOBY', 'GABE', 'MEREDITH', 'ROBERTCALIFORNIA'];
@@ -19,10 +15,15 @@ let gameOfThronesCharacters = ['DANY', 'JONSNOW', 'SANSA', 'ARYA', 'THEON', 'REN
 let bigMouthCharacters = ['JESSI', 'NICK', 'ANDREW', 'DUKEWELLINGTON', 'LEAH', 'JAY', 'MISSY', 'MATTHEW', 'DEVON', 'DEVIN', 'PILLOW', 'COACHSTEVE', 'LOLA', 'HORMONEMONSTER'];
 let thisIsUsCharacters = ['JACK', 'REBECCA', 'KATE', 'RANDALL', 'KEVIN', 'TOBY', 'WILLIAM', 'BETH', 'MIGUEL'];
 
+
+// creates blank word
 function renderState(word) {
   return guessingWord.textContent = [...word].fill("_").join("");
 }
 
+
+
+// updates word if letter is correct
 function updateState(test, word, letter) {
   for (let i = 0; i < word.length; i++) {
     if (word[i] === letter) {
@@ -37,6 +38,8 @@ function updateState(test, word, letter) {
   guessingWord.textContent = test.join("");
 }
 
+
+// shows and hides letters
 function showAndHide(letter) {
   let currentLetter = letter;
   let lowercaseletter = currentLetter.toLowerCase();
@@ -46,6 +49,16 @@ function showAndHide(letter) {
   lowerCase.classList.add('hide');
 }
 
+
+// validates word has been chosen
+function validateWord(){
+  if (word === undefined) {
+    return alert('You must choose a theme first!!');
+  }
+}
+
+
+// starts game
 function startGame(charList) {
   characters = charList;
   word = characters[Math.floor((Math.random() * characters.length))];
@@ -56,10 +69,52 @@ function startGame(charList) {
   currentWord = [...currentState];
   turnsLeft.textContent = 'Turns Left: ' + counter;
 }
+
+function makeVars(alphabet){
+  var variableArray = [];
+  for(let i = 0; i < alphabet.length; i++){
+    let capitalize = alphabet[i].toUpperCase();
+    let lowercase = alphabet[i];
+    capitalize = document.getElementById(`${lowercase}`)
+    array.push(capitalize);
+  }
+  return variableArray;
+}
+
+makeVars('abcdefghijklmnopqrstuvwxyz');
+
+
+function hideLetter(lowercaseLetter){
+  let letter = lowercaseLetter.toUpperCase();
+  validateWord();
+  showAndHide(letter);
+  updateState(currentWord, word, letter);
+
+  turnsLeft.textContent = 'Turns Left: ' + counter;
+
+  if (currentWord.join("") === word){
+    backgroundimg.src = "img/Winner.png";
+    guessingWord.textContent = 'It was ' + word;
+  }
+  if (counter <= 0) {
+    guessingWord.textContent = "You've been bested by: " + word;
+    backgroundimg.src = "img/images.png";
+  } else if (counter < 0){
+    counter = 0;
+  }
+
+}
+
+
+
+// solely for background animation
 window.onanimationend = function(e){
   e.target.classList.remove('animate');
 }
 
+
+
+// starts game with new word for each array of characters
 himym.addEventListener('click', function() {
   startGame(howIMetYourMotherCharacters);
   themes.textContent = 'Current Theme is: How I Met Your Mother';
@@ -96,6 +151,10 @@ thisIsUs.addEventListener('click', function() {
   themes.textContent = 'Current Theme is: This Is Us';
   backgroundimg.classList.add('animate');
 });
+
+
+
+// all code below here affects reset functionality
 
 function resetLetters(){
   a.classList.remove('hide');
